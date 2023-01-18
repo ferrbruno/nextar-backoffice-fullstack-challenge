4916883f8b774a073f4e90836d4022fd649286e2
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-01-18 19:04:22 -0300

    feat: added mongoose module to api :sparkles:

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/nextar-users'),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
