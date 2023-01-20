b3dfd58c814bc320f88d9ce95815d8c5a9fd5c4b
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-01-20 02:29:19 -0300

    feat(common): added user class :sparkles:

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,12 @@
 export enum Permission {
-  admin = 'admin',
-  standard = 'standard'
+  admin = "admin",
+  standard = "standard",
+}
+
+export class User {
+  name!: string;
+  email!: string;
+  phone!: string;
+  permission!: Permission;
+  password!: string;
 }
