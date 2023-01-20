ad53fa36f42e153c378833e8db6fc2eb73b2405f
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-01-20 04:58:20 -0300

    refactor(client): adjusted card styling :recycle:

--- apps/client/src/components/Card/index.tsx
+++ apps/client/src/components/Card/index.tsx
@@ -1,16 +1,12 @@
-import { Inter } from "@next/font/google";
-
 interface CardProps {
   title: string;
   children?: React.ReactNode;
 }
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Card({ title, children }: CardProps) {
   return (
-    <div className="hover:bg-slate-200 transition-transform duration-500 hover:scale-110 py-8 px-8 max-w-md mx-auto rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <h1 className={inter.className}>{title}</h1>
+    <div className="border flex flex-col hover:bg-slate-200 transition-transform duration-500 hover:scale-110 p-8 w-96 max-w-md mx-auto rounded-xl shadow-xl space-y-2 sm:py-4 sm:space-y-0 sm:space-x-6">
+      <h1 className="py-2 text-xl text-center whitespace-nowrap">{title}</h1>
       {children}
     </div>
   );
