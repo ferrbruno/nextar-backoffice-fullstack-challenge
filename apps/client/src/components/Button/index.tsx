0e91e8ca0b270894b373a2a15362115870b55e47
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-01-22 00:40:46 -0300

    refactor(client): changed button component class to theme primary :recycle:

--- apps/client/src/components/Button/index.tsx
+++ apps/client/src/components/Button/index.tsx
@@ -3,10 +3,5 @@ import React, { ButtonHTMLAttributes } from "react";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export default function Button(props: ButtonProps) {
-  return (
-    <button
-      {...props}
-      className="p-2 bg-slate-400 border border-black rounded-md hover:brightness-150"
-    />
-  );
+  return <button className="btn-primary" {...props} />;
 }
