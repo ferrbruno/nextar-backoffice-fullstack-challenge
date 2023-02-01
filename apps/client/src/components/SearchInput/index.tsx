9e961d46ab87f64af65bde45f659dc6768291f95
Bruno Ferreira <bruno.ferr.dev@gmail.com>
2023-02-01 04:19:27 -0300

    feat(client): moved search logic from component to users page :sparkles:

--- apps/client/src/components/SearchInput/index.tsx
+++ apps/client/src/components/SearchInput/index.tsx
@@ -1,13 +1,4 @@
-import { searchUsers } from "@/external/searchUsers";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
-import {
-  ChangeEventHandler,
-  FocusEventHandler,
-  useCallback,
-  useEffect,
-  useState,
-} from "react";
+import { ChangeEventHandler, useCallback, useEffect, useState } from "react";
 import { MagnifyingGlass } from "../icons/MagnifyingGlass";
 
 interface SearchInputProps {
@@ -15,29 +6,12 @@ interface SearchInputProps {
 }
 
 export default function SearchInput({ onSearch }: SearchInputProps) {
-  const router = useRouter();
   const [text, setText] = useState("");
-  const [focused, setFocused] = useState(false);
-
-  const { data } = useQuery({
-    queryKey: ["users", "search", { term: text }],
-    queryFn: () => searchUsers({ name: text, email: text }),
-    enabled: focused && Boolean(text),
-    refetchOnWindowFocus: false,
-  });
 
   const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     (event) => setText(event.target.value),
     []
   );
-
-  const onFocus: FocusEventHandler<HTMLInputElement> = useCallback(() => {
-    setFocused(true);
-  }, []);
-
-  const onBlur: FocusEventHandler<HTMLInputElement> = useCallback(() => {
-    setFocused(false);
-  }, []);
 
   useEffect(() => {
     if (onSearch) {
@@ -46,29 +20,14 @@ export default function SearchInput({ onSearch }: SearchInputProps) {
   }, [onSearch, text]);
 
   return (
-    <div className="relative bg-white border border-black rounded-xl p-1 flex">
+    <div className="relative max-h-10 max-w-xl bg-white border border-black rounded-xl p-1 flex">
       <MagnifyingGlass className="w-6 text-indigo-800" />
       <input
         type="search"
         placeholder="Search"
         className="bg-inherit outline-0 sm:w-96"
         onChange={onChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
       />
-      {data?.length ? (
-        <div className="absolute focus:visible z-10 top-9 right-0 origin-top-left flex flex-col bg-slate-100 border overflow-auto shadow-xl h-fit w-full max-w-96 max-h-96">
-          {...data.map(({ _id, name }) => (
-            <span
-              key={_id}
-              className="w-full border border-transparent hover:border-indigo-800 hover:bg-indigo-200"
-              onClick={() => router.push(`/users/${_id}`)}
-            >
-              {name}
-            </span>
-          ))}
-        </div>
-      ) : null}
     </div>
   );
 }
--- apps/client/src/pages/users/index.tsx
+++ apps/client/src/pages/users/index.tsx
@@ -2,20 +2,26 @@ import Card from "@/components/Card";
 import CardList from "@/components/CardList";
 import { Cog6ToothOutline } from "@/components/icons/Cog";
 import Layout from "@/components/Layout";
+import SearchInput from "@/components/SearchInput";
 import UserInfo from "@/components/UserInfo";
 import { withAuthenticationRequired } from "@/contexts/auth";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { fetchUsers } from "../../external/fetchUsers";
 
 function UsersIndex() {
   const router = useRouter();
+  const [searchValue, setSearchValue] = useState("");
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users"],
     queryFn: fetchUsers,
   });
+
+  const onSearch = useCallback((value: string) => {
+    setSearchValue(value);
+  }, []);
 
   const onClickUser = useCallback(
     (id: string) => () => {
@@ -36,18 +42,29 @@ function UsersIndex() {
 
   return (
     <Layout title="Users">
-      <CardList>
-        {...data.map(({ _id, email, name, phone, permission }) => (
-          <Card key={_id} title={name} onClick={onClickUser(_id)}>
-            <UserInfo
-              email={email}
-              name={name}
-              phone={phone}
-              permission={permission}
-            />
-          </Card>
-        ))}
-      </CardList>
+      <div className="flex flex-col items-center">
+        <SearchInput onSearch={onSearch} />
+        <CardList>
+          {data
+            .filter(({ name, email }) => {
+              if (!searchValue) {
+                return true;
+              }
+
+              return name.includes(searchValue) || email.includes(searchValue);
+            })
+            .map(({ _id, email, name, phone, permission }) => (
+              <Card key={_id} title={name} onClick={onClickUser(_id)}>
+                <UserInfo
+                  email={email}
+                  name={name}
+                  phone={phone}
+                  permission={permission}
+                />
+              </Card>
+            ))}
+        </CardList>
+      </div>
     </Layout>
   );
 }
